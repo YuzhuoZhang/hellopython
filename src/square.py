1fd76e325b21c8a1e09892a739f00b4a016fd15d
zhangyuzhuo <1415068436@qq.com>
2019-12-16 14:10:45 +0800

    分开一下

---
+++ src/square.py
@@ -0,0 +1,22 @@
+import turtle
+
+turtle.pensize(3)
+# turtle.pencolor('blue')
+turtle.pencolor('purple')
+
+# right是顺时针 left是逆时针
+# forword是长度
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+# turtle.right(45)
+# turtle.forward(70.7)
+# turtle.left(90)
+# turtle.right(90)
+# turtle.forward(70.7)
+
+turtle.mainloop()
